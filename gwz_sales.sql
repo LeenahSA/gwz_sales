00f928fbe2b2161d8b5901379092e013d64c3d86
Leenah Saeed <LeenahSaeed0@gmail.com>
2024-03-04 15:35:49 +0300

    Round results
    Round the sum results

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,6 @@
 SELECT 
 date_date,
-ROUND(SUM(turnover),2) AS turnover,
-ROUND(SUM(purchase_cost),2) AS purchase_cost
+ROUND(SUM(turnover),1) AS turnover,
+ROUND(SUM(purchase_cost),1) AS purchase_cost
  FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
  group by date_date
